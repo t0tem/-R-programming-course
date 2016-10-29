30a6fc98c299c589dd399e5976fcc11e47b15fd0
t0tem <vadimkul@gmail.com>
2016-10-29 17:06:21 +0300

    added hierarchical clustering code notes

---
+++ Hier_clustering_notes.R
@@ -0,0 +1,34 @@
+set.seed(1234); par(mar=c(0,0,0,0))
+x <- rnorm(12,mean=rep(1:3,each=4),sd=0.2)
+y <- rnorm(12,mean=rep(c(1,2,1),each=4),sd=0.2)
+plot(x,y,col="blue",pch=19,cex=2)
+text(x+0.05,y+0.05,labels=as.character(1:12))
+
+dataFrame <- data.frame(x=x,y=y)
+distxy <- dist(dataFrame)
+distxy
+hClustering <- hclust(distxy)
+hClustering
+plot(hClustering)
+
+
+# special function from here
+# 
+myplclust <- function( hclust, lab=hclust$labels, lab.col=rep(1,length(hclust$labels)), hang=0.1,...){
+    ## modifiction of plclust for plotting hclust objects *in colour*!
+    ## Copyright Eva KF Chan 2009
+    ## Arguments:
+    ##    hclust:    hclust object
+    ##    lab:        a character vector of labels of the leaves of the tree
+    ##    lab.col:    colour for the labels; NA=default device foreground colour
+    ##    hang:     as in hclust & plclust
+    ## Side effect:
+    ##    A display of hierarchical cluster with coloured leaf labels.
+    y <- rep(hclust$height,2); x <- as.numeric(hclust$merge)
+    y <- y[which(x<0)]; x <- x[which(x<0)]; x <- abs(x)
+    y <- y[order(x)]; x <- x[order(x)]
+    plot( hclust, labels=FALSE, hang=hang, ... )
+    text( x=x, y=y[hclust$order]-(max(hclust$height)*hang),
+          labels=lab[hclust$order], col=lab.col[hclust$order], 
+          srt=90, adj=c(1,0.5), xpd=NA, ... )
+}
