6b72bbc12bf7b3dc6f86cd0ecdc39346ee7e768c
t0tem <vadimkul@gmail.com>
2016-12-26 13:48:05 +0100

    added notes on distributions

---
+++ Distributions.R
@@ -0,0 +1,18 @@
+options(digits = 4)
+
+choose(8, 7) * .5 ^ 8 + choose(8, 8) * .5 ^ 8
+choose(8, 8)
+8 * (8-1) * (8-2) * (8-3) * (8-4) * (8-5) * (8-6)
+k <- 7
+rbinom(10, 2, .5)
+qnorm(0.95, 1, 2)
+pnorm(1.6448, 0, 1, lower.tail = TRUE)
+pnorm(2.6448, 1, 1, lower.tail = TRUE)
+
+pnorm(1160, mean = 1020, sd = 50, lower.tail = FALSE)
+pnorm(2.8, mean = 0, sd = 1, lower.tail = FALSE)
+
+qnorm(0.75, mean = 1020, sd = 50, lower.tail = TRUE)
+
+ppois(3, 2.5*4)
+
