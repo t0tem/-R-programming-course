ea9639868c70bdd8e0a2275e1f4aadf3b4b669a6
t0tem <vadimkul@gmail.com>
2017-03-14 12:06:39 +0300

    added example Shiny apps

--- App2.1/ui.R
+++ App2.1/ui.R
@@ -5,7 +5,8 @@ shinyUI(fluidPage(
         sidebarPanel(
             sliderInput("sliderMPG", "What is the MPG of the car?", 10, 35, value = 20),
             checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
-            checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE)
+            checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE),
+            submitButton("Appply")
         ),
         mainPanel(
             plotOutput("plot1"),
